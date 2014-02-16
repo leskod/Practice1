c8356a6f7900f22dae8b8ab2778b3511cd9c6ca4
Dereck Lesko <lesko@moravian.edu>
2014-02-16 09:53:10 -0500

    Initial Commit

---
+++ Practice1/Practice1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Practice1AppDelegate.h
+//  Practice1
+//
+//  Created by Lesko, Dereck on 2/16/14.
+//  Copyright (c) 2014 Lesko, Dereck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Practice1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Practice1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Practice1
+//
+//  Created by Lesko, Dereck on 2/16/14.
+//  Copyright (c) 2014 Lesko, Dereck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Practice1AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Practice1AppDelegate class]));
+    }
+}
